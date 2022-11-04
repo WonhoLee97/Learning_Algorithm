5c5a55506918a53209dec5ee88dd22fa2cf33761
wono <43237378+WonhoLee97@users.noreply.github.com>
2022-11-05 02:23:50 +0900

    floyd

--- floyd/floyd_3.cpp
+++ floyd/floyd_3.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int W[N][N];
 int D[N][N];
-int P[N][N];
+int P[N][N]={0,};
 
 void floyd(int n, const int W[][N], int D[][N], int P[][N]){
     int i, j, k;
@@ -53,29 +53,29 @@ void path(int q, int r){
 
 int main(void){
     W[0][0]=0;
-	W[0][1]=1;
-	W[0][2]=inf;
-	W[0][3]=1;
+	W[0][1]=inf;
+	W[0][2]=1;
+	W[0][3]=inf;
 	W[0][4]=5;
-	W[1][0]=9;
+	W[1][0]=3;
 	W[1][1]=0;
-	W[1][2]=3;
+	W[1][2]=9;
 	W[1][3]=2;
 	W[1][4]=inf;
-	W[2][0]=inf;
-	W[2][1]=inf;
+	W[2][0]=4;
+	W[2][1]=4;
 	W[2][2]=0;
-	W[2][3]=4;
+	W[2][3]=inf;
 	W[2][4]=inf;
 	W[3][0]=inf;
-	W[3][1]=inf;
-	W[3][2]=2;
+	W[3][1]=8;
+	W[3][2]=6;
 	W[3][3]=0;
-	W[3][4]=3;
-	W[4][0]=3;
+	W[3][4]=inf;
+	W[4][0]=inf;
 	W[4][1]=inf;
 	W[4][2]=inf;
-	W[4][3]=inf;
+	W[4][3]=2;
 	W[4][4]=0;
 	cout<<"Origin Graph"<<endl;
 	print_all(N,W);
@@ -84,9 +84,8 @@ int main(void){
 	print_all(N,D);
 	cout<<"Shortest Path Graph"<<endl;
 	print_all(N,P);
-	path(5,3);
 	cout<<endl;
-	path(3,2);
+	path(1,4);
 	cout<<endl;
 	return 0;
 }
