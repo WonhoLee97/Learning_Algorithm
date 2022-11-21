ccca4d6c97980194f5b474275c4af456ecba18d1
wono <43237378+WonhoLee97@users.noreply.github.com>
2022-11-21 15:06:13 +0900

    Dijkstra

--- Dijkstra/Dijkstra_1.cpp
+++ Dijkstra/Dijkstra_1.cpp
@@ -5,10 +5,13 @@
 using namespace std;
 int F[N];
 int W[N][N]={
-    {0, 0, 0, 0, 0, 0}
-    {0, 0, 7, 4, 6, 1}
-    {0, inf, }
-}
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 7, 4, 6, 1},
+    {0, inf, 0, inf, inf, inf},
+    {0, inf, 2, 0, 5, inf},
+    {0, inf, 3, inf, 0, inf},
+    {0, inf, inf, inf, 1, 0}
+};
 
 void dijkstra(int n, const int W[][N], int F[]){
     int i, vnear, e;
@@ -19,7 +22,7 @@ void dijkstra(int n, const int W[][N], int F[]){
         touch[i]=1;
         length[i]=W[1][i];
     }
-    for(int c=0; c<=n; c++){ //n-1회
+    for(int c=0; c<=n-2; c++){ //n-1회
         int min=inf;
         for(i=2; i<=n; i++){
             if(length[i]>=0 && length[i]<=min){
