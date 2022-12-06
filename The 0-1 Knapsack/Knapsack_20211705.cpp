5a77ac27308c824ba8772a977f2de0dac001d542
wono <43237378+WonhoLee97@users.noreply.github.com>
2022-12-06 22:07:45 +0900

    Knapsack

---
+++ The 0-1 Knapsack/Knapsack_20211705.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+#define N 4
+#define W 16
+
+using namespace std;
+typedef int index;
+
+int w[N+1]={0, 2, 5, 10, 5}; //weight
+int p[N+1]={0, 40, 30, 50, 10}; //profit
+string bestset[N+1];
+string include[N+1];
+int maxprofit;
+int numbest;
+
+   
+bool promising(index i, int profit, int weight){
+    index j,k;
+    int totweight;
+    float bound;
+    if(weight>=W)
+        return false;
+    else{
+        j=i+1;
+        bound=(float)profit;
+        totweight=weight;
+        while(j<=N and totweight+w[j]<=W){
+            totweight=totweight+w[j];
+            bound=bound+p[j];
+            j++;
+        }
+        k=j;
+        if(k<=N){
+            bound=bound+(W-totweight)*p[k]/w[k];
+        }
+        return bound>maxprofit;
+    }
+}
+
+void knapsack(index i, int profit, int weight){
+    if(weight<=W and profit>maxprofit){
+        maxprofit=profit;
+        numbest=i;
+        for(int c=0; c<=N; c++){ //배열 복사
+            bestset[c]=include[c];
+        }
+    }
+
+    if(promising(i,profit,weight)){
+        include[i+1]="yes";
+        knapsack(i+1, profit+p[i+1], weight+w[i+1]);
+        include[i+1]="no";
+        knapsack(i+1, profit, weight);
+    }
+}
+
+
+    
+int main(void){
+    knapsack(0,0,0);
+    cout<<"Maxprofit : "<<maxprofit<<endl;
+    cout<<"Best set"<<endl;
+    for(int c=1; c<=N; c++){
+        cout<<c<<'\t'<<bestset[c]<<endl;
+    }
+    cout<<endl;
+
+}
---
+++ The 0-1 Knapsack/backtracking.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+
+#define N 4
+#define W 7
+
+using namespace std;
+
+double maxprofit = 0.0;
+int p[N+1] = {0, 12, 13, 8, 6};
+int w[N+1] = {0, 5, 6, 4, 3};
+
+vector<string> bestset(N+1);
+vector<string> include(N+1);
+
+bool promising(int i, int profit, int weight){
+	int j, k;
+	int totweight;
+	double bound;
+
+	if (weight >= W) return false;
+
+	else {
+		j = i+1;
+		bound = profit;
+		totweight = weight;
+
+		while ((j <= N) && (totweight + w[j] <= W)){
+			totweight = totweight + w[j];
+			bound = bound + p[j];
+			j++;
+		}
+
+		k = j;
+
+		if (k <= N){
+			bound = bound + (W-totweight)*p[k]/w[k];
+		}
+		return bound > maxprofit;
+	}
+}
+
+void knapsack(int i, int profit, int weight){
+	if ((weight <= W) && (profit > maxprofit)){
+		maxprofit = profit;
+		copy(include.begin(), include.end(), bestset.begin());
+		// bestset.insert(bestset.begin(), include.begin(), include.end());
+	}
+
+	if (promising(i, profit, weight)){
+		include[i+1] = "true";
+		knapsack(i+1, profit+p[i+1], weight+w[i+1]);
+		include[i+1] = "false";
+		knapsack(i+1, profit, weight);
+	}
+}
+
+
+int main (int argc, char* argv[]){
+	// p = {100, 37, 35, 33};
+	// w = {25, 10, 10, 10};
+
+	knapsack(0, 0, 0);
+
+	cout << "maxprofit is : " << maxprofit << endl;
+	for (int i=0; i<N; i++){
+		cout << "\t" << i+1 << " :\t" << bestset[i+1] << endl;
+	}
+
+	return 0;
+}
